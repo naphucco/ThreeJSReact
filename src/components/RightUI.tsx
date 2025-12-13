eea9af8815ebf63a9c1111a7a1322ad49a46ee10
An Phuc <nguyenanphuc92@gmail.com>
2025-12-13 09:31:52 +0700

    Hide panel when unselect

--- src/components/RightUI.tsx
+++ src/components/RightUI.tsx
@@ -13,69 +13,71 @@ function RightUI() {
   const dispatch = useDispatch();
   const selectedTexture = useSelector((state: any) => state.scene.textureImage);
   const transformMode = useSelector((state: any) => state.scene.transformMode); // cho cách đang select (di chuyển/xoay/scale)
+  const selectedItemId = useSelector((state: any) => state.scene.selectedItemId); // 👈 lấy id đang chọn
 
   const handleClick = (imageId: string) => {
     dispatch(setTextureImage(imageId));
   };
 
   return (
-    <div className={styles.containerRight}>
-      <h2 className={styles.title}>🎨 Texture Options</h2>
+    !selectedItemId ? <></> :
+      <div className={styles.containerRight}>
+        <h2 className={styles.title}>🎨 Texture Options</h2>
 
-      {/* Vòng tròn chọn texture */}
-      <div className={styles.textureSection}>
-        <h3 className={styles.subtitle}>Select Texture</h3>
-        <div className={styles.textureList}>
-          {textureOptions.map((option) => (
-            <div
-              key={option.id}
-              onClick={() => handleClick(option.id)}
-              className={`${styles.textureCircle} ${selectedTexture === option.id ? styles.textureCircleSelected : ''
-                }`}
-              style={{ backgroundImage: `url(${option.id})` }}
-              title={option.label}
-            />
-          ))}
+        {/* Vòng tròn chọn texture */}
+        <div className={styles.textureSection}>
+          <h3 className={styles.subtitle}>Select Texture</h3>
+          <div className={styles.textureList}>
+            {textureOptions.map((option) => (
+              <div
+                key={option.id}
+                onClick={() => handleClick(option.id)}
+                className={`${styles.textureCircle} ${selectedTexture === option.id ? styles.textureCircleSelected : ''
+                  }`}
+                style={{ backgroundImage: `url(${option.id})` }}
+                title={option.label}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Chọn TransformControls mode */}
+        <div className={styles.textureSection}>
+          <h3 className={styles.subtitle}>Transform Mode</h3>
+          <div className={styles.transformModeGroup}>
+            <label>
+              <input
+                type="radio"
+                name="mode"
+                value="translate"
+                checked={transformMode === 'translate'}
+                onChange={() => dispatch(setTransformMode('translate'))}
+              />
+              <span>Move</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="mode"
+                value="rotate"
+                checked={transformMode === 'rotate'}
+                onChange={() => dispatch(setTransformMode('rotate'))}
+              />
+              <span>Rotate</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="mode"
+                value="scale"
+                checked={transformMode === 'scale'}
+                onChange={() => dispatch(setTransformMode('scale'))}
+              />
+              <span>Scale</span>
+            </label>
+          </div>
         </div>
       </div>
-
-      {/* Chọn TransformControls mode */}
-      <div className={styles.textureSection}>
-        <h3 className={styles.subtitle}>Transform Mode</h3>
-        <div className={styles.transformModeGroup}>
-          <label>
-            <input
-              type="radio"
-              name="mode"
-              value="translate"
-              checked={transformMode === 'translate'}
-              onChange={() => dispatch(setTransformMode('translate'))}
-            />
-            <span>Move</span>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="mode"
-              value="rotate"
-              checked={transformMode === 'rotate'}
-              onChange={() => dispatch(setTransformMode('rotate'))}
-            />
-            <span>Rotate</span>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="mode"
-              value="scale"
-              checked={transformMode === 'scale'}
-              onChange={() => dispatch(setTransformMode('scale'))}
-            />
-            <span>Scale</span>
-          </label>
-        </div>
-      </div>
-    </div>
   );
 }
 
