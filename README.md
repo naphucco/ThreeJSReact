5c2f1219ce42b4ddb6e12be018832c6d9a31f0ae
An Phuc <nguyenanphuc92@gmail.com>
2025-12-13 15:52:56 +0700

    Complete add model

--- README.md
+++ README.md
@@ -9,18 +9,6 @@ npm install
 # Chạy thử local
 npm run dev
 
-Credit model
-https://sketchfab.com/3d-models/carport-dabed7bcd87a473e9d1bc188ea880564
-https://sketchfab.com/3d-models/low-poly-truck-car-drifter-f3750246b6564607afbefc61cb1683b1"
-https://sketchfab.com/3d-models/double-canopy-carport-b93915526a6a42b0926307d015931c30
-https://sketchfab.com/3d-models/sheed-5be3342732c54d009e566bcd48797892#download
-https://sketchfab.com/3d-models/shed-8be122a8807b46d888ac753b85f25c10#download
-https://sketchfab.com/3d-models/door-2738468b94d74c5f827e7e5df7be8359#download
-https://sketchfab.com/3d-models/double-rusty-russian-garage-d062c8bd476c4fb6b92a78ceb2509291#download
-https://sketchfab.com/3d-models/door-with-frame-2f2f149f3ec44d658a02c1f924dfa449
-https://sketchfab.com/3d-models/graffiti-garage-02-2542852d1d354170bd3f3549a0b75a96
-https://sketchfab.com/3d-models/picture-window-5c7987af188c4b7087003fd76385fade
-https://sketchfab.com/3d-models/pine-tree-e52769d653cd4e52a4acff3041961e65
-https://sketchfab.com/3d-models/small-warehouse-2-1420588322d34ec3b25423bf055398da
-https://sketchfab.com/3d-models/window--wodden-4-mb-1e0ba724fc4f4816bc19be1040ec06fe
-https://sketchfab.com/3d-models/wooden-door-8726b6c219a54ed585e096f267c2a35e#download
+
+
+
--- src/configs/itemOptions.ts
+++ src/configs/itemOptions.ts
@@ -3,27 +3,111 @@ export const itemOptions = [
     id: 'car', label: 'Car',
     scale: 0.005,
     modelUrl: '/models/car.glb',
-    thumbnail: '/thumbnails/car.png'
+    thumbnail: '/thumbnails/car.png',
+    credit: 'https://sketchfab.com/3d-models/low-poly-truck-car-drifter-f3750246b6564607afbefc61cb1683b1'
   },
   {
     id: 'carport',
     label: 'Carport',
     scale: 0.2,
     modelUrl: '/models/carport1.glb',
-    thumbnail: '/thumbnails/carport1.png'
+    thumbnail: '/thumbnails/carport1.png',
+    credit: 'https://sketchfab.com/3d-models/double-canopy-carport-b93915526a6a42b0926307d015931c30'
   },
   {
     id: 'double_canopy_carport',
     label: 'Canopy carport',
     scale: 0.3,
     modelUrl: '/models/double_canopy_carport.glb',
-    thumbnail: '/thumbnails/double_canopy_carport.png'
+    thumbnail: '/thumbnails/double_canopy_carport.png',
+    credit: 'https://sketchfab.com/3d-models/sheed-5be3342732c54d009e566bcd48797892#download'
   },
   {
     id: 'door',
     label: 'Door',
     scale: 0.005,
     modelUrl: '/models/door.glb',
-    thumbnail: '/thumbnails/car.png'
+    thumbnail: '/thumbnails/door.png',
+    credit: 'https://sketchfab.com/3d-models/door-2738468b94d74c5f827e7e5df7be8359#download'
   },
+  {
+    id: 'door_with_frame',
+    label: 'Door with Frame',
+    scale: 0.01,
+    modelUrl: '/models/door_with_frame.glb',
+    thumbnail: '/thumbnails/door_with_frame.png',
+    credit: 'https://sketchfab.com/3d-models/door-with-frame-2f2f149f3ec44d658a02c1f924dfa449'
+  },
+  {
+    id: 'double_rusty_russian_garage',
+    label: 'Rusty Russian Garage',
+    scale: 1,
+    modelUrl: '/models/double_rusty_russian_garage.glb',
+    thumbnail: '/thumbnails/double_rusty_russian_garage.png',
+    credit: 'https://sketchfab.com/3d-models/double-rusty-russian-garage-d062c8bd476c4fb6b92a78ceb2509291#download'
+  },
+  {
+    id: 'graffiti_garage_02',
+    label: 'Graffiti Garage',
+    scale: 0.015,
+    modelUrl: '/models/graffiti_garage_02.glb',
+    thumbnail: '/thumbnails/graffiti_garage_02.png',
+    credit: 'https://sketchfab.com/3d-models/graffiti-garage-02-2542852d1d354170bd3f3549a0b75a96'
+  },
+  {
+    id: 'picture_window',
+    label: 'Picture Window',
+    scale: 0.01,
+    modelUrl: '/models/picture_window.glb',
+    thumbnail: '/thumbnails/picture_window.png',
+    credit: 'https://sketchfab.com/3d-models/picture-window-5c7987af188c4b7087003fd76385fade'
+  },
+  {
+    id: 'pine_tree',
+    label: 'Pine Tree',
+    scale: 0.01,
+    modelUrl: '/models/pine_tree.glb',
+    thumbnail: '/thumbnails/pine_tree.png',
+    credit: 'https://sketchfab.com/3d-models/pine-tree-e52769d653cd4e52a4acff3041961e65'
+  },
+  {
+    id: 'shed',
+    label: 'Shed',
+    scale: 0.5,
+    modelUrl: '/models/shed.glb',
+    thumbnail: '/thumbnails/shed.png',
+    credit: 'https://sketchfab.com/3d-models/shed-8be122a8807b46d888ac753b85f25c10#download'
+  },
+  {
+    id: 'sheed',
+    label: 'Sheed',
+    scale: 0.02,
+    modelUrl: '/models/sheed.glb',
+    thumbnail: '/thumbnails/sheed.png',
+    credit: 'https://sketchfab.com/3d-models/sheed-5be3342732c54d009e566bcd48797892'
+  },
+  {
+    id: 'small_warehouse_2',
+    label: 'Small Warehouse',
+    scale: 0.6,
+    modelUrl: '/models/small_warehouse_2.glb',
+    thumbnail: '/thumbnails/small_warehouse_2.png',
+    credit: 'https://sketchfab.com/3d-models/small-warehouse-2-1420588322d34ec3b25423bf055398da'
+  },
+  {
+    id: 'window__wodden_4_mb',
+    label: 'Wooden Window 4 MB',
+    scale: 1,
+    modelUrl: '/models/window__wodden_4_mb.glb',
+    thumbnail: '/thumbnails/window__wodden_4_mb.png',
+    credit: 'https://sketchfab.com/3d-models/window--wodden-4-mb-1e0ba724fc4f4816bc19be1040ec06fe'
+  },
+  {
+    id: 'wooden_door',
+    label: 'Wooden Door',
+    scale: 0.3,
+    modelUrl: '/models/wooden_door.glb',
+    thumbnail: '/thumbnails/wooden_door.png',
+    credit: 'https://sketchfab.com/3d-models/wooden-door-8726b6c219a54ed585e096f267c2a35e#download'
+  }
 ];
